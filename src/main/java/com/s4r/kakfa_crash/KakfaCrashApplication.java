ce2bf28301e3c3c2e4fe2b6b64a677ee6ef15aa4
saidur-dsi <saidur.rahman@dsinnovators.com>
2024-07-17 23:45:59 +0600

    initialize

---
+++ src/main/java/com/s4r/kakfa_crash/KakfaCrashApplication.java
@@ -0,0 +1,13 @@
+package com.s4r.kakfa_crash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfaCrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfaCrashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s4r/kakfa_crash/KakfaCrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s4r.kakfa_crash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakfaCrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
